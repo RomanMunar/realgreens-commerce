53ff1b8944e0bdb1e00c36f4ddb55930c438304c
Roman Munar <roman.munar1@gmail.com>
2020-12-13 00:29:32 +0800

    added debounce, timeout and intersection hooks

---
+++ lib/hooks/index.ts
@@ -0,0 +1,4 @@
+export { default as useTimeout } from "./useTimeoutFn"
+export { default as useDebounce } from "./useDebounce"
+export { default as useIntersection } from "./useIntersection"
+export { default as getRandomColor } from "./getRandomColor"
