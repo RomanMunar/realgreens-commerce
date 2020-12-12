ace8eff61cfb5f956eac7ef346573dd343f72b85
Roman Munar <roman.munar1@gmail.com>
2020-12-12 11:53:42 +0800

    search route rewrites !! hooray :)

--- next.config.js
+++ next.config.js
@@ -3,4 +3,29 @@ module.exports = {
     locales: ["en-PH", "en-US"],
     defaultLocale: "en-PH",
   },
+  rewites() {
+    return [
+      {
+        source: "/:locale/search",
+        destination: "/search",
+      },
+      {
+        source: "/:locale/search/:path*",
+        destination: "/search",
+      },
+      {
+        source: "/search/vendors/:name",
+        destination: "/search",
+      },
+      {
+        source: "/search/vendors/:name/:category",
+        destination: "/search",
+      },
+      {
+        // This rewrite will also handle `/search/vendors`
+        source: "/search/:category",
+        destination: "/search",
+      },
+    ]
+  },
 }
