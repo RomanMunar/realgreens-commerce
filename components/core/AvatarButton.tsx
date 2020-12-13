50129a83c13a4416b58be87ee58c4c262b12c1fe
Roman Munar <roman.munar1@gmail.com>
2020-12-13 16:41:51 +0800

    avatar button can now displays img hooray

--- components/core/AvatarButton.tsx
+++ components/core/AvatarButton.tsx
@@ -1,9 +1,18 @@
 interface Props {
   onClick?: () => void
+  img?: string | null
 }
 
-const AvatarButton = ({ onClick, ...rest }: Props) => {
-  return (
+const AvatarButton = ({ onClick, img, ...rest }: Props) => {
+  return img && img !== null ? (
+    <button
+      onClick={onClick}
+      {...rest}
+      className="border-2 hover:border-green-900 rounded-full overflow-hidden  inline-flex"
+    >
+      <img src={img} className="w-8 h-8" />
+    </button>
+  ) : (
     <button
       onClick={onClick}
       {...rest}
