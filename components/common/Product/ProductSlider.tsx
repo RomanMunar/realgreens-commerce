6e830b9db0bbba8d3153605090b7ada0a8106dac
Roman Munar <roman.munar1@gmail.com>
2020-12-14 07:37:25 +0800

    slider now uses keen slider, HOORAY

--- components/common/Product/ProductSlider.tsx
+++ components/common/Product/ProductSlider.tsx
@@ -1,58 +1,52 @@
 import cx from "clsx"
+import { useKeenSlider } from "keen-slider/react"
 import { useState } from "react"
 import { Chevron } from "../../icons"
+import "keen-slider/keen-slider.min.css"
 
 const ProductSlider = ({ images }: { images: string[] }) => {
   const [currentSlide, setCurrentSlide] = useState(0)
-  const handleCurrentSlide = (action?: "INC" | "DEC" | number) => {
-    if (typeof action === "number") return setCurrentSlide(action)
-    if (action === "INC") {
-      if (currentSlide === images.length - 1) {
-        return setCurrentSlide(0)
-      }
-      return setCurrentSlide((p) => p + 1)
-    }
-    if (action === "DEC") {
-      if (currentSlide === 0) {
-        return setCurrentSlide(images.length - 1)
-      }
-      return setCurrentSlide((p) => p - 1)
-    }
-  }
+  const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
+    initial: 0,
+    slideChanged(s) {
+      setCurrentSlide(s.details().relativeSlide)
+    },
+    loop: true,
+  })
+
   return (
     <div className="relative h-full w-full">
-      <div
-        className="absolute p-2 w-full h-full flex items-center justify-between"
-        id="arrows"
-      >
-        <button
-          onClick={() => handleCurrentSlide("DEC")}
-          className="active:transform scale-50 transition-transform duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 p-2 rounded-full bg-white"
+      {slider && (
+        <div
+          className="absolute p-2 w-full h-full flex items-center justify-between"
+          id="arrows"
         >
-          <Chevron
-            className={cx(
-              "text-green-400 w-6 h-6 transform transition-transform duration-300 rotate-90"
-            )}
-          />
-        </button>
-        <button
-          onClick={() => handleCurrentSlide("INC")}
-          className="active:transform scale-50 transition-transform duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 p-2 rounded-full bg-white"
-        >
-          <Chevron
-            className={cx(
-              "text-green-400 w-6 h-6 transform transition-transform duration-300 -rotate-90"
-            )}
-          />
-        </button>
-      </div>
+          <button
+            onClick={slider.prev}
+            className="z-1 active:transform scale-50 transition-transform duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 p-2 rounded-full bg-white"
+          >
+            <Chevron className="text-green-400 w-6 h-6 transform transition-transform duration-300 rotate-90" />
+          </button>
+          <button
+            onClick={slider.next}
+            className="z-1 active:transform scale-50 transition-transform duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 p-2 rounded-full bg-white"
+          >
+            <Chevron className="text-green-400 w-6 h-6 transform transition-transform duration-300 -rotate-90" />
+          </button>
+        </div>
+      )}
       <div id="images">
-        <div className="bg-gray-400 max-w-100 block">
-          <img
-            className="sm:h-56 w-100 md:h-96"
-            src={`/products/${images[currentSlide]}.jpg`}
-            alt={images[currentSlide]}
-          />
+        <div
+          ref={sliderRef}
+          className="keen-slider bg-gray-400 max-w-100 block"
+        >
+          {images.map((img) => (
+            <img
+              className="keen-slider__slide h-52 sm:h-56 w-100 md:h-96"
+              src={`/products/${img}.jpg`}
+              alt={img}
+            />
+          ))}
         </div>
       </div>
       <div
@@ -63,7 +57,7 @@ const ProductSlider = ({ images }: { images: string[] }) => {
           images.map((i, idx) => (
             <button
               key={i}
-              onClick={() => handleCurrentSlide(idx)}
+              onClick={() => slider.moveToSlide(idx)}
               className={cx(
                 "p-1 bg-gray-400 border-2 border-white rounded-full",
                 [currentSlide === idx && "bg-green-400"]
