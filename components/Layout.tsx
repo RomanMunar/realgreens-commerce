a9ca81133782e9ab4948386ea09acd786ec51f84
Roman Munar <roman.munar1@gmail.com>
2020-12-13 16:40:57 +0800

    added user and path at the root level

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,18 @@
 import { DialogOverlay, DialogContent } from "@reach/dialog"
+import { User } from "next-auth"
+import { useSession, signIn, signOut } from "next-auth/client"
 import { useRouter } from "next/router"
-import React, { ReactElement, ReactNode } from "react"
+import React, { ReactElement, ReactNode, useEffect, useState } from "react"
 import { useUI } from "../lib/useUI"
 import { CartSidebar, Navbar } from "./common"
 import Footer from "./common/Footer"
-import { LoginModal, SignupModal } from "./core"
+import { ProfileModal, LoginModal, SignupModal } from "./core"
 
 interface Props {
   children: ReactNode
 }
 
-export default function Layout({ children }: Props): ReactElement {
+const Layout = ({ children }: Props): ReactElement => {
   const {
     displaySidebar,
     closeSidebar,
@@ -18,10 +20,24 @@ export default function Layout({ children }: Props): ReactElement {
     modalView,
     closeModal,
   } = useUI()
-  const { locale } = useRouter()
+
+  const [session, loading] = useSession()
+  const [user, setUser] = useState<User | null>(null)
+  useEffect(() => {
+    if (session) {
+      setUser(session.user)
+    } else {
+      setUser(null)
+    }
+  }, [session])
+  const {
+    query: { category: currentPath },
+    locale,
+  } = useRouter()
+
   return (
     <div className="border-t-4 border-green-500">
-      <Navbar />
+      <Navbar locale={locale} currentPath={currentPath} user={user} />
       <DialogOverlay
         className="z-1 fixed top-0 bottom-0 right-0 left-0 overflow-auto bg-opacity-30 bg-black"
         isOpen={displaySidebar}
@@ -38,8 +54,15 @@ export default function Layout({ children }: Props): ReactElement {
           aria-label="Log In Form"
           className="absolute space-y-4 top-1/4 left-1/2 -mt-28 -ml-44 w-96 flex py-10 px-10 flex-col bg-white justify-center items-center"
         >
-          {modalView === "LOGIN_VIEW" && <LoginModal close={closeModal} />}
-          {modalView === "SIGNUP_VIEW" && <SignupModal close={closeModal} />}
+          {modalView === "LOGIN_VIEW" && (
+            <LoginModal loading={loading} signIn={signIn} close={closeModal} />
+          )}
+          {modalView === "SIGNUP_VIEW" && (
+            <SignupModal signIn={signIn} close={closeModal} />
+          )}
+          {modalView === "PROFILE_VIEW" && user && (
+            <ProfileModal signOut={signOut} close={closeModal} user={user} />
+          )}
         </DialogContent>
       </DialogOverlay>
       <main className="min-h-page bg-green-50">{children}</main>
@@ -47,3 +70,5 @@ export default function Layout({ children }: Props): ReactElement {
     </div>
   )
 }
+
+export default Layout
